2168a1e6aa64393b223b6f84ee1f21a9e1cd70b3
Alex Piper <alexkpiper89@gmail.com>
2022-10-17 17:35:31 +0000

    completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'Alex Piper';
+console.log('value of fullName:', fullName);
+var isCool = true;
+console.log('value of isCool:', isCool);
+var totalPets = 1;
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('type of isCool:', typeof isCool);
+console.log('type of totalPets:', typeof totalPets);
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+var never;
+console.log('value of never', never);
+console.log('typeof never', typeof never);
