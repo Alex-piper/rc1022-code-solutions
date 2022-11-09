9d1b22f9f8d7b1a5762129ed77e0ada7878a54b2
Alex Piper <alexkpiper89@gmail.com>
2022-11-09 18:43:31 +0000

    javascript-timers

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,7 +1,7 @@
 var secondsElapsed = 0;
 var $h1 = document.querySelector('.countdown-display');
 
-setInterval(() => {
+var nIntervalID = setInterval(() => {
   secondsElapsed += 1;
   if (secondsElapsed === 1) {
     $h1.textContent = '3';
@@ -14,5 +14,8 @@ setInterval(() => {
   }
   if (secondsElapsed === 4) {
     $h1.textContent = '~Earth Beeeelooowww Us~';
+  }
+  if (secondsElapsed === 5) {
+    clearInterval(nIntervalID);
   }
 }, '1000');
