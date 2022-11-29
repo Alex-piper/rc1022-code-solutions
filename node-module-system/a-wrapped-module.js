1c8b151c91bb3dd78fefe7245458e966f376ec24
Alex Piper <alexkpiper89@gmail.com>
2022-11-29 22:28:57 +0000

    completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,5 @@
+console.log('exports', typeof exports, exports.value, exports);
+console.log('require', typeof require, require.value, require);
+console.log('module', typeof module, module.value, module);
+console.log('__filename', typeof __filename, __filename.value, __filename);
+console.log('__dirname', typeof __dirname, __dirname.value, __dirname);
