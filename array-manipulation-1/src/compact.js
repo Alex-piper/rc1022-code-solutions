59a98d7c4d7494b64c06e8ac7086126882f616dc
Alex Piper <alexkpiper89@gmail.com>
2022-11-30 23:56:36 +0000

    completed array-manipulation-1 exercise

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -3,7 +3,8 @@ function compact(array) {
   var acceptableValues = [];
   var unacceptableValues = [];
   for (var i = 0; i < array.length; i++) {
-    if (array[i] === false || array[i] === '' || array[i] === 0 || array[i] === null || array[i] === undefined) {
+    // eslint-disable-next-line no-self-compare
+    if (array[i] === false || array[i] === '' || array[i] === 0 || array[i] !== array[i] || array[i] === null || array[i] === undefined) {
       unacceptableValues.push(array[i]);
     } else {
       acceptableValues.push(array[i]);
